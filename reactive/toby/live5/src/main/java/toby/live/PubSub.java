723af733e325b47daeaa56be1dfabde81d8e50cd
mins <heroms01@gmail.com>
2019-04-13 15:14:46 +0900

    멀티쓰레드 방식 구현

--- reactive/toby/live5/src/main/java/toby/live/PubSub.java
+++ reactive/toby/live5/src/main/java/toby/live/PubSub.java
@@ -2,12 +2,16 @@ package toby.live;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.Flow.*;
 
 public class PubSub {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
         Publisher publisher = new Publisher() {
             @Override
@@ -17,18 +21,21 @@ public class PubSub {
                 subscriber.onSubscribe(new Subscription() {
                     @Override
                     public void request(long n) {
-                        try {
-                            while (n-- > 0) {
-                                if (it.hasNext()) {
-                                    subscriber.onNext(it.next());
-                                } else {
-                                    subscriber.onComplete();
-                                    break;
+                        executorService.execute(() -> {
+                            int i = 0;
+                            try {
+                                while (i++ < n) {
+                                    if (it.hasNext()) {
+                                        subscriber.onNext(it.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
                                 }
+                            } catch (RuntimeException e) {
+                                subscriber.onError(e);
                             }
-                        } catch (RuntimeException e) {
-                            subscriber.onError(e);
-                        }
+                        });
                     }
 
                     @Override
@@ -44,28 +51,31 @@ public class PubSub {
 
             @Override
             public void onSubscribe(Subscription subscription) {
-                System.out.println("onSubscribe");
+                System.out.println(Thread.currentThread().getName() + " onSubscribe");
                 this.subscription = subscription;
                 this.subscription.request(2);
             }
 
             @Override
             public void onNext(Integer item) {
-                System.out.println("onNext"+item);
-//                this.subscription.request(1);
+                System.out.println(Thread.currentThread().getName() + " onNext "+item);
+                this.subscription.request(1);
             }
 
             @Override
             public void onError(Throwable throwable) {
-                System.out.println("onError");
+                System.out.println(Thread.currentThread().getName() + " onError :" + throwable.getMessage());
             }
 
             @Override
             public void onComplete() {
-                System.out.println("onComplete");
+                System.out.println(Thread.currentThread().getName() + " onComplete");
             }
         };
 
         publisher.subscribe(subscriber);
+
+        executorService.awaitTermination(10, TimeUnit.HOURS);
+        executorService.shutdown();
     }
 }
