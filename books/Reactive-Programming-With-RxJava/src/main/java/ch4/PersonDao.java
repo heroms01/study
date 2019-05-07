cf83e04631ecd3c8e4ed2b1250fd2840d2431029
mins <heroms01@gmail.com>
2019-05-07 22:02:13 +0900

    ch4. 블로킹으로 observable 벗어나기

--- books/Reactive-Programming-With-RxJava/src/main/java/ch4/PersonDao.java
+++ books/Reactive-Programming-With-RxJava/src/main/java/ch4/PersonDao.java
@@ -1,11 +1,19 @@
 package ch4;
 
+import rx.Observable;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class PersonDao {
-    List<Person> listPeople() {
-        return query("SELECT * FROM PEOPLE");
+//    List<Person> listPeople() {
+//        return query("SELECT * FROM PEOPLE");
+//    }
+
+    //기존 API 손상되는 변경
+    Observable<Person> listPeople() {
+        final List<Person> people = query("SELECT * FROM PEOPLE");
+        return Observable.from(people);
     }
 
     private List<Person> query(String s) {
