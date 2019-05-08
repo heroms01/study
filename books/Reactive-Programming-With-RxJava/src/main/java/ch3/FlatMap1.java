311ceb00cda8858d6a03e53b02d7d1d78c46ed2b
mins <heroms01@gmail.com>
2019-05-08 22:11:42 +0900

    RxJava를 활용한 리액티브 프로그래밍 flatMap과 delay

--- books/Reactive-Programming-With-RxJava/src/main/java/ch3/FlatMap1.java
+++ books/Reactive-Programming-With-RxJava/src/main/java/ch3/FlatMap1.java
@@ -5,20 +5,22 @@ import rx.Observable;
 public class FlatMap1 {
     public static void main(String[] args) {
 
-//        Observable<Integer> map1 = Observable.just(1, 2, 3)
-//                .map(i -> i * 2)
-////                .subscribe(System.out::println)
-//        ;
-//
-//        map1.subscribe(System.out::println);
-
-//        Observable<Integer> map2 =
-                Observable.just(1, 2, 3)
-                .flatMap(i -> Observable.just("#"+i, i * 3))
-                .filter(num -> num instanceof String)
-                .subscribe(System.out::println)
+        Observable<Integer> map1 = Observable.just(1, 2, 3)
+                //map은 리턴되는 Integer를 Observable로 감싼다
+                .map(i -> i * 2)
                 ;
+        map1.subscribe(System.out::println);
 
-//        map2.subscribe(System.out::println);
+        Observable<Integer> flatMap1 = Observable.just(1, 2, 3)
+                //flatMap은 Observable을 감싸줘야 한다.
+                .flatMap(i -> Observable.just(i * 3))
+                ;
+        flatMap1.subscribe(System.out::println);
+
+        Observable<?> flatMap2 = Observable.just(1, 2, 3)
+                //flatMap은 다른 Observable로 반환할수도 있다.
+                .flatMap(i -> Observable.just("#"+i, i * 4))
+                ;
+        flatMap2.subscribe(System.out::println);
     }
 }
