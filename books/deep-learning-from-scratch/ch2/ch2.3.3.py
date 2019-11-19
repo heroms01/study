1d77d1d61114612bc0cc459fe974e07c90532451
Mins <heroms01@gmail.com>
2019-11-19 18:20:05 +0900

    딥러닝 - 퍼셉트론과 AND

---
+++ books/deep-learning-from-scratch/ch2/ch2.3.3.py
@@ -0,0 +1,31 @@
+"""
+# 가중치와 편향 도입
+세타를 -b로 치환
+y = 0 = (b + w1*x1 + w2*x2 <= 0)
+y = 1 = (b + w1*x1 + w2*x2 > 0)
+b가 편향(bias)
+"""
+
+import numpy as np
+
+
+def AND(x1, x2):
+	"""
+	세타가 편향으로 치환됨
+	가중치는 각 입력 신호가 결과에 주는 영향력(중요도)을 조절하는 매개변수
+	편향은 뉴런이 얼마나 쉽게 활성화(결과로 1을 출력)하느냐를 조정하는 매개변수
+	"""
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.7
+	tmp = np.sum(w*x) + b
+	if tmp <= 0:
+		return 0
+	else:
+		return 1
+
+
+print(AND(0, 0))
+print(AND(1, 0))
+print(AND(0, 1))
+print(AND(1, 1))
